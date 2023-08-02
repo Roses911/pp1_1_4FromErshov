81a4c4cd22fa830434a27754cab6d4cfe289b6b6
Ilya Ershov <yellowspi@gmail.com>
2023-08-02 09:37:29 +0300

    last commit

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -7,90 +7,85 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-//public class UserDaoJDBCImpl implements UserDao {
-//    private final Connection connection;
-//
-//    public UserDaoJDBCImpl() {
-//      connection = Util.getConectiction();
-//    }
-//
-//    public void createUsersTable() {
-//        String sqlComand = "CREATE TABLE IF NOT EXISTS users (" + "id BIGINT AUTO_INCREMENT PRIMARY KEY," + "name VARCHAR(50) NOT NULL," + "lastName VARCHAR(50) NOT NULL," + "age TINYINT NOT NULL)";
-//        try (Statement statement = connection.createStatement()) {
-//            statement.executeUpdate(sqlComand);
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//
-//    public void dropUsersTable() {
-//        String sqlComand = "DROP TABLE IF EXISTS users";
-//        try (Statement statement = connection.createStatement()) {
-//            statement.executeUpdate(sqlComand);
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            throw new RuntimeException(e);
-//        }
-//
-//    }
-//
-//    public void saveUser(String name, String lastName, byte age) {
-//        String sqlComand = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
-//        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlComand)) {
-//            preparedStatement.setString(1, name);
-//            preparedStatement.setString(2, lastName);
-//            preparedStatement.setByte(3, age);
-//            preparedStatement.executeUpdate();
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            throw new RuntimeException(e);
-//        }
-//
-//    }
-//
-//    public void removeUserById(long id) {
-//        String sqlComand = "DELETE FROM users WHERE id = ?";
-//        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlComand)) {
-//            preparedStatement.setLong(1, id);
-//            preparedStatement.executeUpdate();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
-//    public List<User> getAllUsers() {
-//        List<User> users = new ArrayList<>();
-//        String sqlComand = "SELECT * FROM users";
-//        try (Statement statement = connection.createStatement();
-//             ResultSet resultSet = statement.executeQuery(sqlComand)) {
-//            while (resultSet.next()) {
-//                long id = resultSet.getLong("id");
-//                String name = resultSet.getString("name");
-//                byte age = resultSet.getByte("age");
-//                String lastName = resultSet.getString("lastName");
-//                User user = new User(name, lastName, age);
-//                user.setId(id);
-//                users.add(user);
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            throw new RuntimeException(e);
-//        }
-//        return users;
-//    }
-//
-//    public void cleanUsersTable() {
-//        String sqlComand = "DELETE FROM users";
-//        try (Statement statement = connection.createStatement()) {
-//            statement.executeUpdate(sqlComand);
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//            throw new RuntimeException(e);
-//        }
-//    }
-//}
+public class UserDaoJDBCImpl implements UserDao {
+    private final Connection connection;
+
+    public UserDaoJDBCImpl() {
+      connection = Util.getConnection();
+    }
+
+    public void createUsersTable() {
+        String sqlComand = "CREATE TABLE IF NOT EXISTS users (" + "id BIGINT AUTO_INCREMENT PRIMARY KEY," + "name VARCHAR(50) NOT NULL," + "lastName VARCHAR(50) NOT NULL," + "age TINYINT NOT NULL)";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sqlComand);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void dropUsersTable() {
+        String sqlComand = "DROP TABLE IF EXISTS users";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sqlComand);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        String sqlComand = "INSERT INTO users (name, lastName, age) VALUES (?, ?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlComand)) {
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void removeUserById(long id) {
+        String sqlComand = "DELETE FROM users WHERE id = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlComand)) {
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        String sqlComand = "SELECT * FROM users";
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sqlComand)) {
+            while (resultSet.next()) {
+                long id = resultSet.getLong("id");
+                String name = resultSet.getString("name");
+                byte age = resultSet.getByte("age");
+                String lastName = resultSet.getString("lastName");
+                User user = new User(name, lastName, age);
+                user.setId(id);
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    public void cleanUsersTable() {
+        String sqlComand = "DELETE FROM users";
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sqlComand);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,10 +7,12 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
 
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/rosses";
@@ -52,6 +54,21 @@ public class Util {
         if (sessionFactory != null) {
             sessionFactory.close();
         }
+    }
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+
+
+            connection = DriverManager.getConnection(
+                    URL, USER, PASSWORD);
+            //System.out.println("Подключено");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+
     }
 }
 
